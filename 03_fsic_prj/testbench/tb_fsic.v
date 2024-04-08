a4f47ab10af24b6fd48152776adc60940fb4d239
liyuan-chang <lychang.ee@gmail.com>
2024-04-09 02:26:10 +0800

    add fsic simulation file

--- 03_fsic_prj/testbench/tb_fsic.v
+++ 03_fsic_prj/testbench/tb_fsic.v
@@ -427,14 +427,14 @@ FSIC #(
 		error_cnt = 0;
 		check_cnt = 0;
 
-        testedge();
-		// test001();	//soc cfg write/read test
-		// test002();	//test002_fpga_axis_req
-		// test003();	//test003_fpga_to_soc_cfg_read
-		// test004();	//test004_fpga_to_soc_mail_box_write
-		// test005();	//test005_aa_mailbox_soc_cfg
-		// test006();	//test006_fpga_to_soc_cfg_write
-		// test007();	//test007_mailbox_interrupt test
+        // testedge();
+		test001();	//soc cfg write/read test
+		test002();	//test002_fpga_axis_req
+		test003();	//test003_fpga_to_soc_cfg_read
+		test004();	//test004_fpga_to_soc_mail_box_write
+		test005();	//test005_aa_mailbox_soc_cfg
+		test006();	//test006_fpga_to_soc_cfg_write
+		test007();	//test007_mailbox_interrupt test
 		
 
 
